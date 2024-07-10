3d621b62892c4cf2d6db7a784a102bcd9ed152ea
alex-web-dev <alex.web-dev@mail.ru>
2024-07-10 11:10:34 +0300

    separate file - popup & input-confirm

---
+++ js/input-confirm.js
@@ -0,0 +1,28 @@
+const $confirmInputs = document.querySelectorAll(".input--confirm");
+$confirmInputs.forEach(($input) => {
+  const $field = $input.querySelector(".input__field");
+  const $confirmBtn = $input.querySelector(".input__confirm-btn");
+  const confirmName = $input.dataset.confirmName;
+  const $popup = document.querySelector(`.popup[data-popup-name="${confirmName}"]`);
+
+  $confirmBtn.addEventListener("click", () => {
+    if ($input.classList.contains("input--validated") && $input.classList.contains('NEW_CLASS')) {
+      openPopup($popup);
+      $input.dataset.confirmedValue = $field.value;
+    }
+  });
+});
+
+document.addEventListener("formSuccess", (e) => {
+  const $form = e.detail.form;
+  if (!$form.dataset.confirmName) {
+    return;
+  }
+
+  const $confirmedInput = document.querySelector(`.input[data-confirm-name="${$form.dataset.confirmName}"]`);
+  $confirmedInput.classList.remove("input--error");
+  $confirmedInput.classList.add("input--confirmed");
+
+  const $formPopup = $form.closest(".popup");
+  closePopup($formPopup);
+});
---
+++ js/popup.js
@@ -0,0 +1,66 @@
+const $openBtns = document.querySelectorAll(".js-open-popup");
+$openBtns.forEach(($btn) => {
+  $btn.addEventListener("click", () => {
+    const name = $btn.dataset.popupName;
+    const $popup = document.querySelector(`.popup[data-popup-name="${name}"`);
+    if (!name || !$popup || $popup.classList.contains("popup--active")) {
+      return;
+    }
+
+    openPopup($popup);
+  });
+});
+
+const $popups = document.querySelectorAll(".popup");
+$popups.forEach(($popup) => {
+  $popup.classList.add("popup--show");
+
+  const $closeBtns = $popup.querySelectorAll(".js-close-popup");
+  $closeBtns.forEach(($closeBtn) => {
+    $closeBtn?.addEventListener("click", () => closePopup($popup));
+  });
+
+  const $backdrop = $popup.querySelector(".popup__backdrop");
+  $backdrop?.addEventListener("click", () => closePopup($popup));
+
+  const $dialog = $popup.querySelector(".popup__dialog");
+  $dialog?.addEventListener("click", (e) => {
+    if (e.target === $dialog) {
+      closePopup($popup);
+    }
+  });
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("js-open-popup") || e.target.closest(".js-open-popup")) {
+    return;
+  }
+
+  const $activePopup = document.querySelector(".popup--active.popup--close-outside");
+  const isInner = e.target.closest(".popup") && !e.target.classList.contains("popup");
+  if (!$activePopup || isInner) {
+    return;
+  }
+
+  closePopup($activePopup);
+});
+
+function closePopup($popup) {
+  $popup.classList.remove("popup--active");
+  $popup.addEventListener(
+    "transitionend",
+    () => {
+      if (isLockedBody() && getLockedBodyBy() === `popup-${$popup.dataset.popupName}`) {
+        unlockBody();
+      }
+    },
+    { once: true }
+  );
+}
+
+function openPopup($popup) {
+  $popup.classList.add("popup--active");
+  if (!isLockedBody()) {
+    lockBody(`popup-${$popup.dataset.popupName}`);
+  }
+}
